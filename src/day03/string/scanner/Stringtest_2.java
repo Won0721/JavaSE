0b4d7c7d111c892bc975adfe7c132be1b417a5f3
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-01 09:14:54 +0900

    1

--- src/day03/string/scanner/Stringtest_2.java
+++ src/day03/string/scanner/Stringtest_2.java
@@ -19,8 +19,10 @@ public class Stringtest_2 {
 		
 //		toUpperCase() : 영문자를 대문자로 변환한다.
 		System.out.println("무조건 대문자로 출력 : " + str.toUpperCase());
+		
 //		toLpperCase() : 영문자를 소문자로 변환한다.
 		System.out.println("무조건 대문자로 출력 : " + str.toLowerCase());
+		
 //		charAt(index) : 문자열에서 index 번째 문자 1문자를 얻어온다. 문자의 위치를 계산할 때 0부터
 		System.out.println("3번째 문자 : " + str.charAt(2));
 		sc.close();
